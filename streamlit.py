0fcfbe941b27419faa18c4f758e5b62ea3084591
charlieblindsay <charlieblindsay@gmail.com>
2022-09-29 18:00:37 +0100

    made configurable

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import tensorflow as tf
-import tensorflow_hub as hub
+
+import os
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
 
 from utils import *
 
@@ -15,31 +22,123 @@ def save_uploaded_files(uploaded_files):
 content_image = st.file_uploader("Choose a content image")
 style_image = st.file_uploader("Choose a style image")
 
-@st.cache
-def load_model():
-    return hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
-
 if content_image is not None and style_image is not None:
-    # bytes_data = uploaded_file.read()
-    # st.write("filename:", uploaded_file.name)
-    # st.write(bytes_data)
-
-    # dataframe = pd.read_csv(uploaded_file)
-    # st.dataframe(data=dataframe)
-
-    # file_details = {"FileName":uploaded_file.name,"FileType":uploaded_file.type}
-    # st.write(file_details)
-    # img = load_image(uploaded_file)
-    # st.image(img)
-
-    # content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
-
     save_uploaded_files([{'type': 'content', 'img': content_image}, {'type': 'style', 'img': style_image}])
 
     content_image = load_img('./content.png')
     style_image = load_img('./style.png')
 
-    hub_model = load_model()
-    stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
-    generated_image = tensor_to_image(stylized_image)
+    content_layers = ['block2_conv2'] 
+
+    style_layers = ['block1_conv1',
+                    'block2_conv1',
+                    'block3_conv1', 
+                    'block4_conv1', 
+                    'block5_conv1']
+
+    num_content_layers = len(content_layers)
+    num_style_layers = len(style_layers)
+
+    def load_model():
+        vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
+        vgg.trainable = False
+        return vgg
+    
+    def vgg_layers(layer_names):
+        """ Creates a VGG model that returns a list of intermediate output values."""
+        # Load our model. Load pretrained VGG, trained on ImageNet data
+        vgg = load_model()
+
+        outputs = [vgg.get_layer(name).output for name in layer_names]
+
+        model = tf.keras.Model([vgg.input], outputs)
+        return model
+
+    def gram_matrix(input_tensor):
+        result = tf.linalg.einsum('bijc,bijd->bcd', input_tensor, input_tensor)
+        input_shape = tf.shape(input_tensor)
+        num_locations = tf.cast(input_shape[1]*input_shape[2], tf.float32)
+        return result/(num_locations)
+
+    class StyleContentModel(tf.keras.models.Model):
+        def __init__(self, style_layers, content_layers):
+            super(StyleContentModel, self).__init__()
+            self.vgg = vgg_layers(style_layers + content_layers)
+            self.style_layers = style_layers
+            self.content_layers = content_layers
+            self.num_style_layers = len(style_layers)
+            self.vgg.trainable = False
+
+        def call(self, inputs):
+            "Expects float input in [0,1]"
+            inputs = inputs*255.0
+            preprocessed_input = tf.keras.applications.vgg19.preprocess_input(inputs)
+            outputs = self.vgg(preprocessed_input)
+            style_outputs, content_outputs = (outputs[:self.num_style_layers],
+                                            outputs[self.num_style_layers:])
+
+            style_outputs = [gram_matrix(style_output)
+                            for style_output in style_outputs]
+
+            content_dict = {content_name: value
+                            for content_name, value
+                            in zip(self.content_layers, content_outputs)}
+
+            style_dict = {style_name: value
+                        for style_name, value
+                        in zip(self.style_layers, style_outputs)}
+
+            return {'content': content_dict, 'style': style_dict}
+
+    extractor = StyleContentModel(style_layers, content_layers)
+
+    results = extractor(tf.constant(content_image))
+
+    style_targets = extractor(style_image)['style']
+    content_targets = extractor(content_image)['content']
+
+    image = tf.Variable(content_image)
+
+    def clip_0_1(image):
+        return tf.clip_by_value(image, clip_value_min=0.0, clip_value_max=1.0)
+
+    opt = tf.keras.optimizers.Adam(learning_rate=0.02, beta_1=0.99, epsilon=1e-1)
+
+    style_weight=1e-2
+    content_weight=1e3
+
+    def style_content_loss(outputs):
+        style_outputs = outputs['style']
+        content_outputs = outputs['content']
+        style_loss = tf.add_n([tf.reduce_mean((style_outputs[name]-style_targets[name])**2) 
+                            for name in style_outputs.keys()])
+        style_loss *= style_weight / num_style_layers
+
+        content_loss = tf.add_n([tf.reduce_mean((content_outputs[name]-content_targets[name])**2) 
+                                for name in content_outputs.keys()])
+        content_loss *= content_weight / num_content_layers
+        loss = style_loss + content_loss
+        return loss
+
+    @tf.function()
+    def train_step(image):
+        with tf.GradientTape() as tape:
+            outputs = extractor(image)
+            loss = style_content_loss(outputs)
+
+        grad = tape.gradient(loss, image)
+        opt.apply_gradients([(grad, image)])
+        image.assign(clip_0_1(image))
+
+    epochs = 5 
+    steps_per_epoch = 1
+
+    step = 0
+    for n in range(epochs):
+        for m in range(steps_per_epoch):
+            step += 1
+            train_step(image)
+    
+    generated_image = tensor_to_image(image)
+
     st.image(generated_image)
