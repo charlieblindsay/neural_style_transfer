d0e408472bc1b779f6c842a6c19b68acb15f20df
Charlie Lindsay <charlieblindsay@gmail.com>
2023-02-18 19:19:14 +0000

    updated python library requirements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import tensorflow as tf
-import tensorflow_hub as hub
+import numpy as np
+import PIL
 
 from utils import *
 
@@ -8,17 +9,31 @@ from utils import *
 def load_model():
     return tf.saved_model.load('model')
 
-def save_uploaded_files(uploaded_files):
+
+def save_uploaded_files(uploaded_files: list):
+    """Writes 
+
+    Args:
+        uploaded_files (_type_): _description_
+    """
     for file in uploaded_files:
         file_name = file['type']
-        file_name_extension = f'{file_name}.png'
-        with open(file_name_extension,"wb") as f:
+        file_name_with_extension = f'{file_name}.png'
+        with open(file_name_with_extension,"wb") as f:
             f.write(file['img'].getbuffer())
 
 st.title('Neural Style Transfer Demo')
 st.write('After the user has uploaded a content image and a style image, an image with the \'content\' of the content image and the \'style\' of style image is generated.')
 
 st.subheader('File upload')
+
+st.write(np.__version__)
+st.write(PIL.__version__)
+st.write(tf.__version__)
+st.write(st.__version__)
+
+
+
 
 content_image = st.file_uploader("Choose a content image. This should be a photo.")
 style_image = st.file_uploader('Choose a style image. This should be a piece of art, e.g. a painting')
