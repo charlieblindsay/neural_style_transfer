c0dcb070d1e35e2958fdccc1731f232ceeb273c2
Charlie Lindsay <charlieblindsay@gmail.com>
2023-02-19 18:38:03 +0000

    created tests folder

--- test_utils.py
+++ test_utils.py
@@ -9,8 +9,8 @@ class TestUtils(unittest.TestCase):
         """Function which tests whether the image_tensor generated using load_img_tensor_from_path function
         is the equal to the actual image tensor.
         """
-        test_image_tensor = utils.load_img_tensor_from_path('./test_image.png')
-        actual_test_image_tensor = tf.convert_to_tensor(np.load('actual_test_image_tensor.npy'), dtype=tf.float32)
+        test_image_tensor = utils.load_img_tensor_from_path('./tests/test_image.png')
+        actual_test_image_tensor = tf.convert_to_tensor(np.load('./tests/actual_test_image_tensor.npy'), dtype=tf.float32)
         self.assertTrue(tf.math.reduce_all(tf.equal(test_image_tensor,actual_test_image_tensor)).numpy())
 
 if __name__ == '__main__':
